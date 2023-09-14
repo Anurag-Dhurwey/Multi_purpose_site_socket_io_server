13b20738fbf9f19a4c563015232f3411dd59d211
Anurag-Dhurwey <anuragdhurwey9211@gmail.com>
2023-09-14 13:30:49 +0530

    now only connected user will emit 'allOnlineUsers'

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ const app = express();
 const server = http.createServer(app);
 
 const origin = process.env.ACCEPTED_CORS_ORIGINS;
-// this is comment
+
 const io = new Server(server, {
   cors: {
     origin: [origin ? origin : ""],
@@ -38,7 +38,24 @@ io.on("connection", async (socket) => {
       );
       if (!isUseralreadyAvalable) {
         onlineUsers.push({ ...msg.user, socketId: socket.id });
-        io.emit("allOnlineUsers", onlineUsers);
+        onlineUsers?.forEach((usr) => {
+          const { connections, socketId } = usr;
+          const isExist = connections?.connected.find((conUsr) => {
+            return conUsr.user._id == msg.user._id;
+          });
+          if (isExist || usr.email==msg.user.email) {
+            const connectedOnlineUsr = onlineUsers.filter((Onusr) => {
+              const isConnected = connections?.connected?.find(
+                (conUsr) => {
+                  return Onusr._id == conUsr.user._id;
+                }
+              );
+              return isConnected !=undefined;
+            });
+            io.to(socketId).emit("allOnlineUsers", connectedOnlineUsr);
+          }
+        });
+        // io.emit("allOnlineUsers", connectedOnlineUsr);
         console.log("onHnadshake");
       }
     } else {
@@ -47,22 +64,41 @@ io.on("connection", async (socket) => {
   });
 
   socket.on("ConnectionRequest", (msg: onconnectionrequestMsg) => {
-    const { sentBy,sendTo, _key } = msg;
-    console.log({ _key });
-
-    socket
-      .to(sendTo.socketId)
-      .emit("ConnectionRequestToUser", { user: sentBy, _key });
+    const { sentBy, sendTo, _key } = msg;
+    const isOnline = onlineUsers.find((usr) => {
+      return usr.email == sendTo.email;
+    });
+    if (isOnline) {
+      console.log({ _key });
+      socket
+        .to(isOnline.socketId)
+        .emit("ConnectionRequestToUser", { user: sentBy, _key });
+    }
   });
 
   socket.on("chat_message", (msg: chat_message) => {
-    socket.to(msg.receiver_socketId).emit("chat_message",msg)
+    socket.to(msg.receiver_socketId).emit("chat_message", msg);
   });
 
   socket.on("disconnect", () => {
-    onlineUsers = onlineUsers.filter((user) => user.socketId !== socket.id);
+    const disconnectingUsr = onlineUsers.find(
+      (user) => user.socketId == socket.id
+    );
+
+    onlineUsers?.forEach((usr) => {
+      const { connections, socketId } = usr;
+      const isExist = connections?.connected.find((conUsr) => {
+        return conUsr.user._id == disconnectingUsr?._id;
+      });
+      if (isExist) {
+        const newCopyOfOnlineUsers = onlineUsers?.filter(
+          (user) => user.socketId !== socket.id && user.socketId != socketId
+        );
+        io.to(socketId).emit("allOnlineUsers", newCopyOfOnlineUsers);
+      }
+    });
+    onlineUsers = onlineUsers?.filter((user) => user.socketId !== socket.id);
     console.log(`${socket.id} : disconnected`);
-    io.emit("allOnlineUsers", onlineUsers);
   });
 });
 
--- src/typescript/main.ts
+++ src/typescript/main.ts
@@ -1,14 +1,26 @@
-export interface onlineUser{
-    socketId:string;
-    _id:string;
-    name:string;
-    email:string;
-    image:string
+export interface onlineUser {
+  socketId: string;
+  _id: string;
+  name: string;
+  email: string;
+  image: string;
+  connections: { connected: usr_and_key_in_array[] };
 }
 
-export interface onhandshakeMsg{
-    user:onlineUser
+export interface onhandshakeMsg {
+  user: {
+    _id: string;
+    name: string;
+    email: string;
+    image: string;
+    connections: { connected: usr_and_key_in_array[] };
+  };
 }
+
+type usr_and_key_in_array = {
+  _key: string;
+  user: { _id: string; name: string; email: string; image: string };
+};
 // interface user extends onlineUser{
 //     connections: {
 //         connected: usersMinData[];
@@ -21,26 +33,21 @@ export interface onhandshakeMsg{
 //     link: string | null;
 //     socketId:string;
 // }
-export interface onconnectionrequestMsg{
-    sendTo:onlineUser;
-    sentBy:onlineUser;
-    _key:string
+export interface onconnectionrequestMsg {
+  sendTo: { _id: string; name: string; email: string; image: string };
+  sentBy: onlineUser;
+  _key: string;
 }
 
-export type usersMinData = {
-    _key: string;
-    userId: string;
-    name: string;
-    email: string;
-    image: string;
-  };
-
-
- export interface chat_message {
-  sender_id: string;
-  receiver_id: string;
+export interface chat_message {
+  sender: _ref;
+  receiver: _ref;
   message: string;
   date_time: Date;
-  receiver_email:string;
-  receiver_socketId:string;
+  // receiver_email: string;
+  receiver_socketId: string;
 }
+export interface _ref{
+  _type: string;
+  _ref: string;
+};
